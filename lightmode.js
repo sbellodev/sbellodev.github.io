9e8b9d0e44dfcd73fdafe2146fb6db76b4786db7
Santi <sbellodev@gmail.com>
2022-05-02 21:50:35 +0200

    refactor files

---
+++ lightmode.js
@@ -0,0 +1,56 @@
+let darkmodeCount = 0;
+let darkmode_btn = document.getElementsByClassName("darkmode-btn")[0];
+darkmode_btn.addEventListener("click", function setDarkmode() {
+  let bodyEl = document.getElementsByTagName("body")[0];
+  let navEl = document.getElementsByTagName("nav")[0];
+  let contactLogos = document.getElementsByClassName("logos");
+  let submit = document.querySelectorAll("input[type=submit]")[0];
+  let colorBlack = "rgb(24, 24, 24)";
+  let colorWhite = "floralwhite";
+
+  if (darkmodeCount < 5) {
+    darkmodeCount < 1
+      ? (darkmode_btn.innerHTML = "No please :(")
+      : darkmodeCount < 2
+      ? (darkmode_btn.innerHTML = "Oh no...")
+      : darkmodeCount < 3
+      ? (darkmode_btn.innerHTML = "No, no, no...")
+      : darkmodeCount < 4
+      ? (darkmode_btn.innerHTML = "Don't do this!")
+      : (darkmode_btn.innerHTML = "!!!!");
+
+    setTimeout(() => (darkmode_btn.innerHTML = "LightMode"), 1000);
+    darkmodeCount++;
+  } else {
+    if (darkmode_btn.innerHTML === "LightMode") {
+      darkmode_btn.innerHTML = "DarkMode";
+      bodyEl.style.backgroundColor = colorWhite;
+      bodyEl.style.color = colorBlack;
+      bodyEl.style.fontWeight = "bold";
+      navEl.style.backgroundColor = "white";
+      navEl.style.color = colorBlack;
+      navEl.style.borderBottom = "2px solid " + colorBlack;
+      contactLogos[0].src = "/img/twitter_reverse.svg";
+      contactLogos[1].src = "/img/github_reverse.svg";
+      contactLogos[2].src = "/img/linkedin_reverse.svg";
+      contactLogos[3].src = "/img/mail_reverse.svg";
+      submit.style.color = colorBlack;
+      submit.style.border = "2px solid " + colorBlack;
+      submit.style.backgroundColor = "white";
+    } else if (darkmode_btn.innerHTML === "DarkMode") {
+      darkmode_btn.innerHTML = "LightMode";
+      bodyEl.style.backgroundColor = colorBlack;
+      bodyEl.style.color = colorWhite;
+      bodyEl.style.fontWeight = "normal";
+      navEl.style.backgroundColor = colorBlack;
+      navEl.style.borderBottom = "2px solid " + colorWhite;
+      contactLogos[0].src = "/img/twitter.svg";
+      contactLogos[1].src = "/img/github.svg";
+      contactLogos[2].src = "/img/linkedin.svg";
+      contactLogos[3].src = "/img/mail.svg";
+      submit.style.color = "white";
+      submit.style.border = "4px solid white";
+      submit.style.backgroundColor = colorBlack;
+    }
+  }
+});
